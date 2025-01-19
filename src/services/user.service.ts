f5192a7277d8fa354bc0e879b4505443550a987b
Andrey Dantas <andreydantasvf@gmail.com>
2025-01-19 19:41:35 -0300

    feat: add errors

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,12 +1,22 @@
 import { hash } from 'bcrypt';
 import { prisma } from '../plugins/prisma';
+import { AppError } from '../errors/AppError';
 
 export async function createUser(data: {
   name: string;
   email: string;
   password: string;
 }) {
+  const userExists = await prisma.user.findFirst({
+    where: { email: data.email }
+  });
+
+  if (userExists) {
+    throw new AppError('User already exists', 400);
+  }
+
   const hashedPassword = await hash(data.password, 10);
+
   return prisma.user.create({
     data: { ...data, password: hashedPassword }
   });
