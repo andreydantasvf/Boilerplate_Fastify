740fa451c84387ad285af1ec727d94b6d486418f
Andrey Dantas <andreydantasvf@gmail.com>
2025-01-19 16:58:56 -0300

    feat: update file

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,6 @@ module.exports = {
   // Lint & Prettify TS and JS files
   '*.{js,ts}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
-    `npm run lint --fix ${filenames.join(' ')}`,
-    `npm run test -- --findRelatedTests ${filenames.join(' ')}`
+    `npm run lint --fix ${filenames.join(' ')}`
   ]
 };
