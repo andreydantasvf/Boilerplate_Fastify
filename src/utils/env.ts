43e72e5f14f98b579fb8fd3a0e3c1e30f9029d39
Andrey Dantas <andreydantasvf@gmail.com>
2025-01-19 19:41:06 -0300

    feat: use default values

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  NODE_ENV: z.string(),
-  PORT: z.coerce.number(),
-  DATABASE_URL: z.string(),
-  JWT_SECRET: z.string()
+  NODE_ENV: z.string().default('development'),
+  PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string().default(''),
+  JWT_SECRET: z.string().default('secret')
 });
 
 export const env = envSchema.parse(process.env);
